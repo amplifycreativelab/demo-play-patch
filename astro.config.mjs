da6499c70ce83c7348b69a188353f5941a52d523
Spaghetti <stefanomocci92@gmmail.com>
2025-12-27 22:43:21 +0800

    Configure Astro for GitHub Actions deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://github.com/amplifycreativelab",
-  base: "demo1",
+  site: "https://amplifycreativelab.github.io",
+  base: "/demo-play-patch",
   vite: {
     plugins: [tailwindcss()]
   },
